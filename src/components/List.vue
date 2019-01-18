46acc1ba6f525a4713231895628dd80aff3d6d81
GXQi <15809237114@163.com>
2019-01-18 17:02:14 +0800

    编写商品列表

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="list">
-    111
+    <div class="header">
+        <h1>列表</h1>
+    </div>
   </div>
 </template>
 
@@ -8,6 +10,13 @@
 import axios from 'axios'
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      infoList: [],
+      totalPrice: 0.00,
+      totalNum: 0
+    }
+  },
   methods: {
     getInfo () {
       axios.get('static/mock/index.json').then(this.getInfoSucc)
@@ -31,4 +40,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
+.list
+  .header
+    padding: 10px 0
+    background: #26a2ff
+    font-size: 20px
+    color: #FFF
+    h1
+      text-align: center
+      font-weight: 600
 </style>
